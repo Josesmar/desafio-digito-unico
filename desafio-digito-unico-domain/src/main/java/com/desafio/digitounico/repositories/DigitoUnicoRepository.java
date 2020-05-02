9213d498336c168813cba058d392a1ff47892e98
jessicacrislm <jessicacris.lm@gmail.com>
2020-05-02 18:55:55 -0300

    [DU-04] Desenvolvimento dos repositórios

--- desafio-digito-unico-domain/src/main/java/com/desafio/digitounico/repositories/DigitoUnicoRepository.java
+++ desafio-digito-unico-domain/src/main/java/com/desafio/digitounico/repositories/DigitoUnicoRepository.java
@@ -1,5 +1,20 @@
 package com.desafio.digitounico.repositories;
 
-public class DigitoUnicoRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.desafio.digitounico.entities.DigitoUnico;
+
+@Repository
+public interface DigitoUnicoRepository extends JpaRepository<DigitoUnico, Long> {
+	
+	@Query("SELECT DISTINCT du FROM DigitoUnico du WHERE du.usuario.id = (:idUsuario")
+	public List<DigitoUnico> findAllByUsuario(@Param("idUsuario") Long idUsuario); 
+	
+	@Query("SELECT DISTINCT (CASE WHEN du.id is null THEN false ELSE true END) FROM DigitoUnico du WHERE du.id = (:id)")
+	public boolean isValid(@Param("id") Long id);
 }
--- desafio-digito-unico-domain/src/main/java/com/desafio/digitounico/repositories/UsuarioRepository.java
+++ desafio-digito-unico-domain/src/main/java/com/desafio/digitounico/repositories/UsuarioRepository.java
@@ -1,5 +1,27 @@
 package com.desafio.digitounico.repositories;
 
-public class UsuarioRepository {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.desafio.digitounico.entities.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	@Override
+	@Query("SELECT DISTINCT u FROM Usuario u LEFT JOIN FETCH u.digitos")
+	public List<Usuario> findAll();
+	
+	@Override
+	@Query("SELECT DISTINCT u FROM Usuario u LEFT JOIN FETCH u.digitos WHERE u.id = (:id)")
+	public Optional<Usuario> findById(@Param("id") Long id);
+
+	@Query("SELECT DISTINCT (CASE WHEN u.id IS NULL is null THEN false ELSE true END) FROM Usuario u WHERE u.id = (:id)")
+	public boolean isValid(@Param("id") Long id);
 
 }
